2ce2b445ce95c05861d3d7beea22712c361ed799
deveripon <developershahadathussainripon@gmail.com>
2024-04-23 01:01:29 +0600

    big

--- components/details/DetailsSection.jsx
+++ components/details/DetailsSection.jsx
@@ -5,15 +5,16 @@ const DetailsSection = () => {
                 <h2 className='font-bold text-2xl'>Details</h2>
                 <div className='my-2 text-[#AEAEAE] space-y-4 prose lg:prose-lg max-w-none'>
                     <p className=''>
-                        If you're passionate about data, coding, and everything
-                        tech-related, this is an event you won't want to miss.
-                        Whether you're a seasoned developer or just getting
-                        started, join us to discuss the latest trends in data
-                        analysis, programming, and software development. This is
-                        a fantastic opportunity to chat with fellow tech
-                        enthusiasts, exchange ideas, and maybe even spark some
-                        brilliant collaborations. Hosted in the 2nd Floor
-                        Training Room at The Power Plant Business Incubator.
+                        If you{" ' "}re passionate about data, coding, and
+                        everything tech-related, this is an event you won{" ' "}
+                        t want to miss. Whether you{" ' "}re a seasoned
+                        developer or just getting started, join us to discuss
+                        the latest trends in data analysis, programming, and
+                        software development. This is a fantastic opportunity to
+                        chat with fellow tech enthusiasts, exchange ideas, and
+                        maybe even spark some brilliant collaborations. Hosted
+                        in the 2nd Floor Training Room at The Power Plant
+                        Business Incubator.
                     </p>
 
                     <ul className=''>
